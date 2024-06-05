adfe4b02aae45dfff823ccd80cfc9fc0121d4f69
Muchammad Rifki Maulana <mrifkimaulana1998@gmail.com>
2024-06-05 16:06:49 +0700

    add view/view-remove-to-do.php

---
+++ view/view-remove-to-do.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once __DIR__ . "/../helper/input.php";
+require_once __DIR__ . "/../business-logic/remove-to-do.php";
+
+function viewRemoveToDo(): void
+{
+    echo "\n- remove -\n";
+    $choice = input("number (press 'x' to cancel)");
+    echo "\n";
+
+    if (strtolower($choice) === "x") {
+        echo "- cancel remove -\n\n";
+    } else if ((int)$choice || $choice === "0") {
+        $success = removeToDo($choice);
+
+        echo "- " . ($success ? "successfully" : "failed") . " remove to do number $choice -\n\n";
+    } else {
+        echo "- option not understood -\n\n";
+    }
+}
